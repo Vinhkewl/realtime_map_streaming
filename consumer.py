1ee1a93672d62ed3f87b21c2fcc4e5a10ac837ea
PC-08 <vinhnguyenthe78.work@gmail.com>
2024-03-05 17:22:03 +0700

    organized function

--- consumer.py
+++ consumer.py
@@ -7,15 +7,23 @@ from fastapi import Request
 from fastapi import WebSocket
 from fastapi.templating import Jinja2Templates
 
+import json
+from kafka import KafkaConsumer
+
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-
+KAFKA_SERVER = 'localhost:9092'
+TOPIC = 'coordinates'
 
 with open('measurements.json', 'r') as file:
-    measurements = iter(json.loads(file.read()))
+    measurements = json.loads(file.read())
 
+consumer = KafkaConsumer(TOPIC,
+                             bootstrap_servers=KAFKA_SERVER,
+                             api_version=(0, 11, 5),
+                             value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
 @app.get("/")
 def read_root(request: Request):
@@ -26,7 +34,8 @@ def read_root(request: Request):
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
-        await asyncio.sleep(10000)
-        payload = next(measurements)
-        logger.info(payload)
-        await websocket.send_json(payload)
+        for message in consumer:
+            measurements = json.loads(message.value)
+            await asyncio.sleep(0.1)
+            logger.info(measurements)
+            await websocket.send_json(measurements)
---
+++ producer.py
@@ -0,0 +1,104 @@
+import json
+import folium
+import time
+import re
+import requests
+from google.transit import gtfs_realtime_pb2
+from json import dumps
+from kafka import KafkaProducer
+from kafka.admin import KafkaAdminClient, NewTopic
+from kafka import KafkaConsumer
+
+try:
+    admin_client = KafkaAdminClient(
+        bootstrap_servers="localhost:9092",
+        client_id='tracking'
+    )
+
+    topic_list = [NewTopic(name="coordinates", num_partitions=1, replication_factor=1)]
+    admin_client.create_topics(new_topics=topic_list, validate_only=False)
+except:
+    pass
+
+api_url = 'https://realtime.hsl.fi/realtime/vehicle-positions/v2/hsl'  # Replace with your API endpoint
+KAFKA_SERVER = 'localhost:9092'
+TOPIC = 'coordinates'
+
+
+def fetch_coordinates():
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        # Fetch the GTFS-RT payload
+        response = requests.get(api_url)
+        payload = response.content
+
+        # Create a GTFS-RT feed message
+        feed = gtfs_realtime_pb2.FeedMessage()
+
+        # Parse and decode the payload
+        feed.ParseFromString(payload)
+
+        # Create a list to hold the decoded entities
+        decoded_entities = []
+
+        # Iterate over the feed entities
+        for entity in feed.entity:
+            # Convert the entity to a dictionary
+            if re.search(r'47/668', entity.id):
+                entity_dict = {
+                    'id': entity.id,
+                    'vehicle': {
+                        'trip': {
+                            'route_id': entity.vehicle.trip.route_id,
+                            'start_time': entity.vehicle.trip.start_time,
+                            'start_date': entity.vehicle.trip.start_date
+                            # Add other trip attributes as needed
+                        },
+                        'position': {
+                            'latitude': entity.vehicle.position.latitude,
+                            'longitude': entity.vehicle.position.longitude,
+                            'bearing': entity.vehicle.position.bearing
+                            # Add other trip attributes as needed
+                        },
+                        'position': {
+                            'latitude': entity.vehicle.position.latitude,
+                            'longitude': entity.vehicle.position.longitude,
+                            'bearing': entity.vehicle.position.bearing,
+                            'odometer': entity.vehicle.position.odometer,
+                            'speed' : entity.vehicle.position.speed
+                            # Add other trip attributes as needed
+                        },
+                        'stop_id': entity.vehicle.stop_id,
+                        'current_status': entity.vehicle.current_status,
+                        'timestamp': entity.vehicle.timestamp
+                    }
+                }
+
+
+                # Append the entity dictionary to the list
+                decoded_entities.append(entity_dict)
+
+        # Convert the list of decoded entities to a JSON array
+        json_array = json.dumps(entity_dict)
+        # print(type(json_array))
+        return json_array
+    else:
+        print(f"Error fetching coordinates: {response.status_code}")
+        return None
+
+
+def produce_kafka_messages():
+    producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER, 
+                             api_version=(0,11,5),
+                             value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+
+    while True:
+        coordinates = fetch_coordinates()
+        if coordinates:
+            producer.send(TOPIC, value=coordinates)
+            print(f"Message produced: {coordinates}")
+
+        time.sleep(2)  # Wait for 5 seconds before fetching coordinates again
+
+if __name__ == "__main__":
+    produce_kafka_messages()
